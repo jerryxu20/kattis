0274715b8c99754c4ad46121a88851bfed42ca1b
jerryxu20 <xu_jerry@outlook.com>
2023-07-15 11:18:24 -0600

    fixed

--- numbertree.cpp
+++ numbertree.cpp
@@ -1,29 +1,74 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+typedef long long ll;
+typedef long double ld;
+typedef complex<ld> cd;
 
-int main() {
-    int h;
-    string s = "";
-    cin >> h;
-    h += 1;
-    while (cin >> s) {
-        continue;
-    }
-    int n = 1 << h;
-    vector<int> tree(n);
-    int num = 1;
-    for (int i = n - 1; i >= 1; i--) {
-        tree[i] = num++;
-    }
-    int idx = 1;
-    for (char c: s) {
-        if (c == 'L') {
-            idx = idx * 2;
-        } else {
-            idx = idx * 2 + 1;
+typedef pair<int, int> pi;
+typedef pair<ll,ll> pl;
+typedef pair<ld,ld> pd;
+
+typedef vector<int> vi;
+typedef vector<ld> vld;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<cd> vcd;
+typedef vector<bool> vb;
+typedef tuple<int,int,int> ti;
+typedef vector<string> vs;
+typedef vector<double> vd;
+typedef vector<vi> vii;
+typedef vector<vii> viii;
+
+template<class T> using PQ = priority_queue<T>;
+template<class T> using PQG = priority_queue<T, vector<T>, greater<T>>;
+
+#define rep(i, a, b) for (int i=a; i<(b); i++)
+#define FOR(i, a) for (int i=0; i<(a); i++)
+#define FORd(i,a,b) for (int i = (b)-1; i >= a; i--)
+#define F0Rd(i,a) for (int i = (a)-1; i >= 0; i--)
+#define trav(x,A) for (auto& x : A)
+
+#define sz(x) (int)(x).size()
+#define all(x) x.begin(), x.end()
+#define mp make_pair
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+#define ins insert
+
+const int MOD = 1000000007;
+const char nl = '\n';
+
+int solve(int tt) {
+    int n;
+    string s;
+    cin >> n;
+    while(cin >> s);
+    int node = 1;
+    for (char &c: s) {
+        node *= 2;
+        if (c == 'R') {
+            node++;
         }
     }
-    cout << tree[idx];
+    // cout << node << nl;
+    cout << (1 << (n + 1)) - node << nl;
+
+    tt++;
+    return 0;
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    // cin.exceptions(cin.failbit);
+    int T = 1;
+    // cin >> T;
+    for (int i = 1; i <= T; i++) {
+        if (solve(i)) break;
+    }
+    T++;
     return 0;
 }
