5b83f5a8588cb964a75e5ef95eb1fb1f490963d2
jerryxu20 <xu_jerry@outlook.com>
2024-02-06 18:05:42 -0700

    solves

---
+++ studentsko.cpp
@@ -0,0 +1,143 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef complex<ld> cd;
+
+typedef pair<int, int> pi;
+typedef pair<ll,ll> pl;
+typedef pair<ld,ld> pd;
+
+typedef vector<int> vi;
+typedef vector<ld> vld;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<cd> vcd;
+typedef vector<bool> vb;
+typedef tuple<int,int,int> ti;
+typedef vector<string> vs;
+typedef vector<double> vd;
+typedef vector<vi> vii;
+typedef vector<vii> viii;
+
+template<class T> using PQ = priority_queue<T>;
+template<class T> using PQG = priority_queue<T, vector<T>, greater<T>>;
+
+#define rep(i, a, b) for (int i=a; i<(b); i++)
+#define FOR(i, a) for (int i=0; i<(a); i++)
+#define FORd(i,a,b) for (int i = (b)-1; i >= a; i--)
+#define F0Rd(i,a) for (int i = (a)-1; i >= 0; i--)
+#define trav(x,A) for (auto& x : A)
+
+#define sz(x) (int)(x).size()
+#define all(x) x.begin(), x.end()
+#define mp make_pair
+#define pb push_back
+#define lb lower_bound
+#define ub upper_bound
+#define ins insert
+
+const int MOD = 1000000007;
+const char nl = '\n';
+
+const int inf = 1e9;
+struct Node {
+	Node *l = 0, *r = 0;
+	int lo, hi, mset = inf, madd = 0, val = inf;
+	Node(int lo,int hi):lo(lo),hi(hi){} // Large interval of -inf
+	Node(vi& v, int lo, int hi) : lo(lo), hi(hi) {
+		if (lo + 1 < hi) {
+			int mid = lo + (hi - lo)/2;
+			l = new Node(v, lo, mid); r = new Node(v, mid, hi);
+			val = min(l->val, r->val);
+		}
+		else val = v[lo];
+	}
+	int query(int L, int R) {
+		if (R <= lo || hi <= L) return inf;
+		if (L <= lo && hi <= R) return val;
+		push();
+		return min(l->query(L, R), r->query(L, R));
+	}
+	void set(int L, int R, int x) {
+		if (R <= lo || hi <= L) return;
+		if (L <= lo && hi <= R) mset = val = x, madd = 0;
+		else {
+			push(), l->set(L, R, x), r->set(L, R, x);
+			val = min(l->val, r->val);
+		}
+	}
+	void add(int L, int R, int x) {
+		if (R <= lo || hi <= L) return;
+		if (L <= lo && hi <= R) {
+			if (mset != inf) mset += x;
+			else madd += x;
+			val += x;
+		}
+		else {
+			push(), l->add(L, R, x), r->add(L, R, x);
+			val = min(l->val, r->val);
+		}
+	}
+	void push() {
+		if (!l) {
+			int mid = lo + (hi - lo)/2;
+			l = new Node(lo, mid); r = new Node(mid, hi);
+		}
+		if (mset != inf)
+			l->set(lo,hi,mset), r->set(lo,hi,mset), mset = inf;
+		else if (madd)
+			l->add(lo,hi,madd), r->add(lo,hi,madd), madd = 0;
+	}
+};
+
+int solve(int tt) {
+    int n, k;
+    cin >> n >> k;
+
+    vi num(n), idx(n);
+    trav(x, num) cin >> x;
+
+    iota(all(idx), 0);
+    sort(all(idx), [&] (auto &a, auto &b) {
+        return num[a] < num[b];
+    });
+
+    rep(i, 0, n) {
+        num[idx[i]] = i/k; 
+    }
+
+    Node dp(0, n);
+
+    dp.set(0, 1, 0);
+
+    rep(i, 0, n) {
+        int x = num[i];
+
+        dp.set(x, x + 1, dp.query(0, x + 1));
+
+        // removing x
+        dp.add(0, x, 1);
+        dp.add(x + 1, n, 1);
+    }
+
+    cout << dp.query(0, n) << nl;
+
+
+    tt++;
+    return 0;
+}
+
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    cin.exceptions(cin.failbit);
+    int T = 1;
+    // cin >> T;
+    for (int i = 1; i <= T; i++) {
+        if (solve(i)) break;
+    }
+    T++;
+    return 0;
+}
